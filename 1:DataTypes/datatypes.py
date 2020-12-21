5cf063c2752697195eb3474b0ffc239977236cb3
An Nguyen <annguyen@Ans-MacBook-Pro.local>
2020-12-21 00:37:18 -0500

    First lesson

---
+++ 1:DataTypes/datatypes.py
@@ -0,0 +1,63 @@
+"""
+Data type and operators
+There are a a couple main data types in Python:
+1. Text type: str
+2. Numeric type: int, float
+3. Sequence type: list, tuple, range
+4. Mapping type: dict
+5. Boolean type: True, False
+"""
+
+# 1: text type
+# Anything that goes in to this "" or '' is a string
+# E.g:
+
+"Hello Trang Beo"
+"3"
+"4"
+"Du ma"
+# You can use the print() fundtion to display the string in the console
+# String is a immutable data, meaning you cannot change its content once it's being declared
+# You can concadinate, meaning you can add two or more strings together using the + operator
+# E.g:
+print("An is " + "awesome :D")
+# This prints: An is awesome. 
+
+# 2: Numeric type
+# Integer or decimal (float) numbers 
+# E.g (int):
+3
+2
+6
+146862493
+8758146
+1
+-1545
+-7
+#E.g (float):
+1.45
+2.6567
+716249.2356
+"""
+You can use math operators for calculations in Python
+Some basic operators:
+1. Addition: +
+2. Subtraction: -
+3. Multiplication: *
+4. Division: /
+5. Exponential: **
+
+There are also some special operators:
+1. Remainder: % 
+This operator returns the remainder of a division (eg: 4 % 3 = 1)
+2. Assignment operator: =
+This operator is use to declare a variable (eg: x = 3)
+3. Comparision operators: 
+  a, == : This is to check if two data is equal, if they do return True, else returns False
+  b, > : This is to check if a data is bigger
+  c, < : This is to check if a data is smaller
+  d, >= : This is to check if a data is bigger or equal
+  e, <= : This is to check if a data is smaller or equal
+  f, != : TThis is to check if two data is not equal, if they do return True, else returns False
+"""
+
