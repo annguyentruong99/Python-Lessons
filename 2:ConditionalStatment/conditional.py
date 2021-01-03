b873818f6fe8bddc5b530b25305c0b2d3ee7fdd9
An Nguyen <annguyen@ans-macbook-pro.myfiosgateway.com>
2021-01-02 23:23:20 -0500

    Second Meeting

--- 2:ConditionalStatment/conditional.py
+++ 2:ConditionalStatment/conditional.py
@@ -1,5 +1,21 @@
-number = int(input("Enter your number: "))
-if number % 2 == 0 or number != 0:
-  print("This is an even number")
-elif number % 2 == 1:
-  print("Odd")
+# number = int(input("Enter your number: "))
+# if (number % 2 == 0) or (number != 0):
+#   print("This is an even number")
+# elif (number % 2 == 1):
+#   print("Odd")
+
+
+# # Rock paper scissor game
+# player1 = input("You choose Rock, Paper, or Scissor? ")
+# player2 = input("You choose Rock, Paper, or Scissor? ")
+
+# a = [1,2,7,8]
+# a[2:2] = [3,4,5,6]
+# print(a)
+
+a = 12
+b = 10
+a = b + b*2
+b = a + 10 
+c = a + b
+print()
